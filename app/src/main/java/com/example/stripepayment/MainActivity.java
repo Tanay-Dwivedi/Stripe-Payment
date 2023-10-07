f275c2be643680fa7d036c5ae8d332245aec47f9
Tanay-Dwivedi <91426152+Tanay-Dwivedi@users.noreply.github.com>
2023-10-08 01:26:21 +0530

    basic ui and logic done

--- app/src/main/java/com/example/stripepayment/MainActivity.java
+++ app/src/main/java/com/example/stripepayment/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.stripepayment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText amt;
+    Button payBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        amt = findViewById(R.id.amtValue);
+        payBtn = findViewById(R.id.button);
+
+        payBtn.setOnClickListener(v -> {
+            String s = amt.getText().toString().trim();
+            //long rAmt = Long.parseLong(s);
+
+            if (s.length() == 0) {
+                Toast.makeText(MainActivity.this, "Wrong Payment Value", Toast.LENGTH_SHORT).show();
+            } else {
+                if(Long.parseLong(s)==0) {
+                    Toast.makeText(MainActivity.this, "zero amount can't be payed", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                }
+            }
+        });
+
     }
 }
